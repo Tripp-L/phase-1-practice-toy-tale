edc818e546619577a047f8dfb65a6b31f28e8eff
trippl <tlaursen8@yahoo.com>
2024-04-16 19:22:11 -0400

    Toy Tale

--- src/index.js
+++ src/index.js
@@ -1,68 +1,84 @@
 let addToy = false;
-const toyCollection = document.getElementById("toy-collection")
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
-  });
-// <div class="card">
-//  <h2>Woody</h2>
-//  <img src="[toy_image_url]" class="toy-avatar" />
-//  <p>4 Likes</p>
-//  <button class="like-btn" id="[toy_id]">Like ❤️</button>
-// </div>
-})
-const createCard = (toy) => {
-  //createCard(curToy)
-  //create card for current toy
-  //create needed nodes
-  let toyCard = document.createElement("div");
-  let toyName = document.createElement("h2");
-  let toyImage = document.createElement("img");
-  let toyLikes = document.createElement("p");
-  let toyButton = document.createElement("button");
-
-  cardDiv.className = "card"
-  toyName.textContent = toy.name;
-  toyImage.src = toy.image;
-  toyImage.className = "toy-avatar";
-   if(toy.Likes === 1) {
-    toyLikes.textContent = `${toy.Likes} like`
-   } else {
-    toyLikes.textContent = `${toy.Likes} likes`
-   }
-  toyLikes.textContent = toy.likes;
-  toyButton.textContent = "Like-btn";
-  toyButton.id = toy.id;
-
-  //append node appropriately
-  toyCard.appendChild(toyName)
-  toyCard.appendChild(toyImage)
-  toyCard.appendChild(toyLikes)
-  toyCard.appendChild(toyButton)
-
-  //append card to toyCollection
-toyCollection.append(toyCard)
-}
-
-const fetchToys = (endpoint) => {
-  return fetch(`http://localhost:3000/${endpoint}`) //fetch result gets returned to next .then
-  .then(res => res.json()) //res.json() result gets returned to next .then
-  //.then(data => data) 
-}
-
-fetchToys("toys")
-.then(toys => {
-
-//fetchToys returns a promise, so we still need an additional .then
-//.then(toys => { //equivalent 
-  //for elements...
-  //1. create them
-  //2. write needed data (textContent, src, className, id)
-  //append them to something
-  console.log(toys)
-    toys.forEach(curToy => {
-    createCard(curToy)
-    })
+    // hide & seek with the form
+    addToy = !addToy;
+    if (addToy) {
+      toyFormContainer.style.display = "block";
+    } else {
+      toyFormContainer.style.display = "none";
+    }
   })
+})
+
+fetch("http://localhost:3000/toys")
+  .then((resp) => resp.json())
+  .then((data) => renderToys(data))
+
+function renderToys(toyArr) {
+
+  const toyContainer = document.querySelector('#toy-collection')
+
+  toyArr.forEach((toyObj) => {
+
+    console.log(toyObj)
+
+    const card = document.createElement('div')
+    card.className = 'card'
+
+    const h2 = document.createElement('h2')
+    h2.textContent = toyObj.name
+    
+    const img = document.createElement('img')
+    img.src = toyObj.image
+    img.className = 'toy-avatar'
+    
+    const p = document.createElement('p')
+    p.textContent = toyObj.likes + " likes"
+    
+    const btn = document.createElement('button')
+    btn.className = 'like-btn'
+    btn.id = toyObj.id
+    btn.textContent = 'Like ❤️'
+
+    let currLikes = toyObj.likes
+
+    btn.addEventListener('click', handleIncrementLikes)
+
+    function handleIncrementLikes() {
+      currLikes++ // currLikes = currLikes + 1
+      p.textContent = `${currLikes} likes`
+    }
+
+    
+    card.appendChild(h2)
+    card.appendChild(img)
+    card.appendChild(p)
+    card.appendChild(btn)
+
+    toyContainer.appendChild(card)
+
+  })
+
+}
+
+const form = document.querySelector('.add-toy-form')
+
+form.addEventListener('submit', (e) => handleAddNewToy(e))
+
+function handleAddNewToy(e) {
+  e.preventDefault()
+  
+  const newToyObj = {
+    name : e.target.name.value,
+    image : e.target.image.value,
+    likes : 0,
+    id : 0
+  }
+
+  renderToys([newToyObj])
+
+}
